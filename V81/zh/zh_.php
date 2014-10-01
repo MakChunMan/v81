ed383150ce708ec5ccf62d90b740837b9c43906e
MAk <MAk@MAk-PC>
2014-10-01 22:13:35 +0800

    [2014-10-01] Init V81

---
+++ V81/zh/zh_.php
@@ -0,0 +1,9 @@
+﻿<?
+$MSG["zh"]["Please Login"] = "客戶登入";
+$MSG["zh"]["Your password.."] = "密碼";
+$MSG["zh"]["Remember Me?"] = "3個月免登入";
+$MSG["zh"]["Your email.."] = "電郵";
+$MSG["zh"]["Login Submit"] = "登入";
+$MSG["zh"]["Forgot your password?"] = "忘記密碼?";
+$MSG["zh"]["Create new account"] = "註冊新用戶";
+?>
